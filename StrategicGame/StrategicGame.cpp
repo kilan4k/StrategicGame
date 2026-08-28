df109ecfbc24b160e9e283f33df3d03be308f1ed
kilan <kilan22042009@gmail.com>
2026-08-28 11:41:12 +0300

    Добавьте файлы проекта.

---
+++ StrategicGame/StrategicGame.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    
+
+
+
+}
+
+/* How detailed i should make those things from 1 to 5
+Economy — 3.67
+Military / Warfare — 4.33
+Diplomacy — 2.33
+Politics — 3.00
+Society / Population — 3.33
+Technology / Development — 3.33
+World / Events — 2.67
+*/
