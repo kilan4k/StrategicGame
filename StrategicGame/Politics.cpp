8a24dded335a92f91ebc1aed5545f6545cd70e9e
kilan <kilan22042009@gmail.com>
2026-09-16 20:54:55 +0300

    feat: implement getters for Politics class

--- StrategicGame/Politics.cpp
+++ StrategicGame/Politics.cpp
@@ -1,1 +1,27 @@
 #include "Politics.h"
+
+int Politics::getIdeologyId()const { return ideologyId; }
+void Politics::setIdeologyId(int Id){}
+
+int Politics::getGovernmentId()const { return governmentId; }
+void Politics::setGovernmentId(int Id){}
+
+int Politics::getStability()const { return stability; }
+void Politics::setStability(int value){}
+
+int Politics::getCorruptionRate()const { return corruptionRate; }
+void Politics::setCorruptionRate(int value){}
+
+int Politics::getGovernmentApproval()const { return governmentApproval; }
+void Politics::setGovernmentApproval(int value){}
+
+int Politics::getTurnsUntilElections()const { return turnsUntilElections; }
+void Politics::setTurnsUntilElections(int turns){}
+
+int Politics::getWarSupport()const { return warSupport; }
+void Politics::setWarSupport(int value){}
+
+bool Politics::getHasElections()const { return hasElections; }
+void Politics::setHasElections(bool value){}
+
+
