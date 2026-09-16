75a3fd3cea4e81ab2b88dc1ccc18fc91d21fffbd
kilan <kilan22042009@gmail.com>
2026-09-16 19:10:57 +0300

    feat: Console class printEconomy method

--- StrategicGame/Console.cpp
+++ StrategicGame/Console.cpp
@@ -1,1 +1,49 @@
 #include "Console.h"
+#include <iostream>
+using std::cout;
+void Console::printMessage(std::string message){
+	cout << message;
+}
+void Console::beautifyOutput() {
+	cout << "--------------------\n";
+}
+void Console::printCountry(const Country& country) {
+	
+
+
+}
+void Console::printEconomy(const Economy& economy) {
+	beautifyOutput();
+	cout << "Economy statistics\n";
+	beautifyOutput();
+	cout << "\tGDP: " << economy.getGdp() << "$\n";
+	cout << "\tBudget: " << economy.getBudget() << "$\n";
+	cout << "\tTax revenue: " << economy.getTaxRevenue() << "$\n";
+	beautifyOutput();
+	cout << "\tEconomic growth rate: " << economy.getEconomicGrowthRate() << "%\n";
+	cout << "\tInflation rate: " << economy.getInflationRate() << "%\n";
+	cout << "\tKey rate: " << economy.getKeyRate() << "%\n";
+	cout << "\tUnemployment rate: " << economy.getUnemploymentRate() << "%\n";
+	beautifyOutput();
+	cout << "\tGovernment spending: " << economy.getGovernmentSpending() << "$\n";
+	cout << "\tSocial spending: " << economy.getSocialSpendingPerc() << "%\n";
+	cout << "\tMilitary spending: " << economy.getMilitarySpendingPerc() << "%\n";
+	cout << "\tEducation spending: " << economy.getEducationSpendingPerc() << "%\n";
+	cout << "\tHealthcare spending: " << economy.getHealthcareSpendingPerc() << "%\n";
+	beautifyOutput();
+}
+void Console::printMilitary(const Military& military) {
+
+}
+void Console::printPolitics(const Politics& politics) {
+
+
+}
+void Console::printSociety(const Society& society) {
+
+
+}
+void Console::printEvent(const Events& event) { //later
+
+
+}
--- StrategicGame/Console.h
+++ StrategicGame/Console.h
@@ -1,15 +1,26 @@
 #pragma once
+#include "Country.h"
+#include "Economy.h"
+#include "Military.h"
+#include "Politics.h"
+#include "Society.h"
+#include "Events.h"
+#include "Technology.h"
+#include <string>
+
 class Console
 {
 private:
 
-
-
-
 public:
-
-
-
+	void printMessage(std::string message);
+	void beautifyOutput();
+	void printCountry(const Country& country);
+	void printEconomy(const Economy& economy);
+	void printMilitary(const Military& military);
+	void printPolitics(const Politics& politics);
+	void printSociety(const Society& society);
+	void printEvent(const Events& event);
 
 
 
