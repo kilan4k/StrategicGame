3825c64fa429628df2649de096f72b3796668dea
kilan <kilan22042009@gmail.com>
2026-09-18 20:09:34 +0300

    feat: implement some basic methods for society.cpp

--- StrategicGame/Society.cpp
+++ StrategicGame/Society.cpp
@@ -1,1 +1,55 @@
 #include "Society.h"
+
+
+long Society::getPopulation()const { return population; }
+void Society::setPopulation(long value) {
+	if (value > 0) {
+		population = value;
+	}
+	else {
+		population = 0;
+	}
+}
+
+float Society::getPopulationGrowth()const { return populationGrowth; }
+void Society::setPopulationGrowth(float value) { populationGrowth = value; }
+
+int Society::getMigrants()const { return migrants; }
+void Society::setMigrants(int value) {
+	if (value >= 0) {
+		migrants = value;
+	}
+	else {
+		migrants = 0;
+	}
+}
+
+short Society::getLiteracyRate()const { return literacyRate; }
+void Society::setLiteracyRate(short value) {
+	if (value >= 0 and value <= 100) {
+		literacyRate = value;
+	}
+}
+
+
+short Society::getInequalityRate()const { return inequalityRate; }
+void Society::setInequalityRate(short value) {
+	if (value >= 0 and value <= 100) {
+		inequalityRate = value;
+	}
+}
+
+short Society::getHealthcareLevel()const { return healthcareLevel; }
+void Society::setHealthcareLevel(short value) {
+	if (value >= 0 and value <= 100) {
+		healthcareLevel = value;
+	}
+}
+
+
+short Society::getCrimeRate()const { return crimeRate; }
+void Society::setCrimeRate(short value) {
+	if (value >= 0 and value <= 100) {
+		crimeRate = value;
+	}
+}
--- StrategicGame/Society.h
+++ StrategicGame/Society.h
@@ -3,18 +3,35 @@ class Society
 {
 private:
 	long population;
+	float populationGrowth;
 	int migrants;
 
 	short literacyRate;
-	short educationLevel;
-	short healthcareLevel;
-
 	short inequalityRate;
+	short healthcareLevel;
 	short crimeRate;
 
 
+public:
+	long getPopulation()const;
+	void setPopulation(long value);
 
+	float getPopulationGrowth()const;
+	void setPopulationGrowth(float value);
 
+	int getMigrants()const;
+	void setMigrants(int value);
 
+	short getLiteracyRate()const;
+	void setLiteracyRate(short value);
+
+	short getInequalityRate()const;
+	void setInequalityRate(short value);
+
+	short getHealthcareLevel()const;
+	void setHealthcareLevel(short value);
+
+	short getCrimeRate()const;
+	void setCrimeRate(short value);
 };
 
