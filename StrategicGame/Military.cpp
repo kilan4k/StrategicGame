3386c78cfcf8db4985c8d7495a748e3251e83694
kilan <kilan22042009@gmail.com>
2026-09-20 21:22:14 +0300

    feat: implement basic methods in Military.cpp

--- StrategicGame/Military.cpp
+++ StrategicGame/Military.cpp
@@ -1,1 +1,21 @@
 #include "Military.h"
+
+int Military::getManpower()const { return manpower; }
+void Military::addManpower(int amount) { manpower += amount; }
+void Military::reduceManpower(int amount) { manpower -= (manpower-amount)>0 ?amount : 0; }
+
+int Military::getUnitAmount(UnitType type)const {
+	auto it = units.find(type);
+	if (it != units.end()) {
+		return it->second;
+	}
+	return 0;
+
+}
+void Military::addUnit(UnitType type, int amount){
+	units[type] += amount;
+}
+void Military::removeUnit(UnitType type, int amount) {
+	units[type] -= amount;
+}
+
