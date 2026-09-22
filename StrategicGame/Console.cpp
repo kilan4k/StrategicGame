58b64bda08b5fec625a9d5ce3d459654f6456692
kilan <kilan22042009@gmail.com>
2026-09-22 18:39:18 +0300

    feat: implement printMilitary method

--- StrategicGame/Console.cpp
+++ StrategicGame/Console.cpp
@@ -1,4 +1,5 @@
 #include "Console.h"
+#include "ArmyUnit.h"
 #include <iostream>
 using std::cout;
 void Console::printMessage(std::string message){
@@ -36,9 +37,13 @@ void Console::printMilitary(const Military& military) {
 	beautifyOutput();
 	cout << "Military Menu\n";
 	beautifyOutput();
-
-
-
+	cout << "\tManpower: " << military.getManpower() << "\n\n";
+	for (const auto& unit : UnitStats) {
+		if (military.getUnitAmount(unit.unitType) > 0) {
+			cout << "\t" << unit.name << ": " << military.getUnitAmount(unit.unitType) << " units\n";
+		}
+	}
+	beautifyOutput();
 }
 void Console::printPolitics(const Politics& politics) {
 	const Ideology* ideology = getIdeologyByEnum(politics.getIdeology());
@@ -54,11 +59,12 @@ void Console::printPolitics(const Politics& politics) {
 		cout << "\tIdeology: " << "Unkown Ideology\n";
 	}
 	if (government) {
-		cout << "\tGovernment type: " << government->governmentName<<"\n";
+		cout << "\tGovernment type: " << government->governmentName << "\n";
 	}
 	else {
-		cout << "\tGovernment type: " << "Unkown Government type\n";
+		cout << "\tGovernment type: " << "Unknown Government type\n";
 	}
+	beautifyOutput();
 	cout << "\tStability rate: " << politics.getStability() << "%\n";
 	cout << "\tWar support rate: " << politics.getWarSupport() << "%\n";
 	cout << "\tCorruption rate: " << politics.getCorruptionRate() << "%\n";
@@ -69,6 +75,7 @@ void Console::printPolitics(const Politics& politics) {
 	else {
 		cout << "\tNo elections" << "\n";
 	}
+	beautifyOutput();
 }
 void Console::printSociety(const Society& society) {
 	beautifyOutput();
@@ -77,10 +84,11 @@ void Console::printSociety(const Society& society) {
 	cout << "\tPopulation: " << society.getTotalPopulation() << "\n";
 	cout << "\tPopulation growth: " << society.getPopulationGrowth() << "%\n";
 	cout << "\tMigrants: " << society.getMigrants() << "\n";
+	beautifyOutput();
 	cout << "\tLiteracy Rate: " << society.getLiteracyRate() << "%\n";
 	cout << "\tInequality Rate: " << society.getInequalityRate() << "%\n";
 	cout << "\tCrime Rate: " << society.getCrimeRate() << "%\n";
-
+	beautifyOutput();
 }
 void Console::printEvent(const Events& event) { //later
 
